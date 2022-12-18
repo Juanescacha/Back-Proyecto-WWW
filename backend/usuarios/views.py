b99c7272efd7505b266446e99a7a3aa26bb264fe
Juanescacha <juanescacha@hotmail.com>
2022-12-18 13:07:06 -0500

    CRUD para usuarios

--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import UsuarioSerializer
+from .models import Usuario
 
-# Create your views here.
+
+class UsuarioView(viewsets.ModelViewSet):
+    serializer_class = UsuarioSerializer
+    queryset = Usuario.objects.all()
