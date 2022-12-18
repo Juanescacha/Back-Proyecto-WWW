c962a05e7655f54f43314b58006cd7112cce1989
Juanescacha <juanescacha@hotmail.com>
2022-12-18 13:10:04 -0500

    pequeno cambio al import para que podamos impotar los demas modelos

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from usuarios import views
+from usuarios.views import UsuarioView
 
 router = routers.DefaultRouter()
-router.register(r"usuarios", views.UsuarioView, "usuario")
+router.register(r"usuarios", UsuarioView, "usuario")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
