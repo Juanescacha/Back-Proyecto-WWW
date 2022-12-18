b7746bd6ac287f1f38ee41abc1a701dfab309162
Juanescacha <juanescacha@hotmail.com>
2022-12-18 13:35:27 -0500

    CRUD for Posts

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from rest_framework import routers
 from usuarios.views import UsuarioView
 from productos.views import ProductoView
+from posts.views import PostView
 
 router = routers.DefaultRouter()
 router.register(r"usuarios", UsuarioView, "usuario")
 router.register(r"productos", ProductoView, "producto")
+router.register(r"posts", PostView, "post")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
