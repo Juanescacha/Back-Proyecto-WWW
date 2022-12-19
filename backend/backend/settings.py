ae078553e29a54f080a8fc6675fab7ba997ed725
Juanescacha <juanescacha@hotmail.com>
2022-12-18 21:03:32 -0500

    test postgres db remota

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "Zi8SHqQkuXODvnMtUTUD",
+        "HOST": "containers-us-west-173.railway.app",
+        "PORT": "7481",
     }
 }
 
