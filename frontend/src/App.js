682b0d294a0589acce2e624f74e5e227b6008846
Juanescacha <juanescacha@hotmail.com>
2022-12-24 01:07:15 -0500

    react router base config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
 import "style.css"
 import "tailwindcss/lib/css/preflight.css"
-import AnimationRevealPage from "helpers/AnimationRevealPage"
+import GlobalStyles from "styles/GlobalStyles"
+import { css } from "styled-components/macro" //eslint-disable-line
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+const Dev = () => {
+  return <div>Test</div>
+}
 
 const App = () => {
-  return <AnimationRevealPage></AnimationRevealPage>
+  return (
+    <>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dev />} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,9 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App"
+import Modal from "react-modal"
+
+Modal.setAppElement("#root")
 
 const container = document.getElementById("root")
 const root = createRoot(container)
