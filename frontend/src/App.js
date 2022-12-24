93f8e9d0a408e7dbb181e00d18c41e0f9e24d399
Juanescacha <juanescacha@hotmail.com>
2022-12-23 23:09:34 -0500

    finalizacion de configuracion inicial de plantilla

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
+import "style.css"
+import "tailwindcss/lib/css/preflight.css"
+import AnimationRevealPage from "helpers/AnimationRevealPage"
+
 const App = () => {
-  return <div>Hola</div>
+  return <AnimationRevealPage></AnimationRevealPage>
 }
 
 export default App
