7bd928eda267ca1c54633631055fb66ce1712cf4
timetravel-1010 <timetravel_1010@protonmail.com>
2022-12-29 10:26:33 -0500

    Implementar modelo y realizar migraciones de usuarios

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class User(models.Model):
+   
+    ROLE_CHOICES = [
+        ('AD', 'administrator'),
+        ('AS', 'assistant'),
+        ('CL', 'client'),
+    ]
+
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=50)
+    role = models.CharField(
+        max_length=2,  
+        choices=ROLE_CHOICES,
+        default='CL'
+    )
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
---
+++ usuarios/urls.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+from .api import UserViewSet
+
+router = routers.DefaultRouter()
+
+router.register('api/users', UserViewSet, 'users')
+
+urlpatterns = router.urls
