f603b237277f255175f44eb2c28038e87a8895ae
timetravel-1010 <timetravel_1010@protonmail.com>
2023-01-01 09:55:39 -0500

    Eliminar backend anterior

--- backend/productos/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import ProductoSerializer
-from .models import Producto
-
-
-class ProductoView(viewsets.ModelViewSet):
-    serializer_class = ProductoSerializer
-    queryset = Producto.objects.all()
--- backend/usuarios/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Usuario
-
-
-class UsuarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Usuario
-        fields = ("id", "nombre", "email", "password", "url_imagen", "tipo", "estado")
