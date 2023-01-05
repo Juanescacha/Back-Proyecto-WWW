b3f75381bcf11302e94015e4682fb916c766d672
timetravel-1010 <53354136+timetravel-1010@users.noreply.github.com>
2023-01-04 20:18:52 -0500

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
   'http://localhost:8000',
   'http://localhost:3000',
