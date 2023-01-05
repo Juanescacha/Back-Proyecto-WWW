ab0466557c611705d8643240cadfe4889b733274
timetravel-1010 <53354136+timetravel-1010@users.noreply.github.com>
2023-01-04 20:33:23 -0500

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -66,11 +66,12 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
+"""
 CORS_ORIGIN_WHITELIST = (
   'http://localhost:8000',
   'http://localhost:3000',
 )
-
+"""
 ROOT_URLCONF = 'api.urls'
 
 TEMPLATES = [
