c3da1d4aa274a7e6b1853e19c39f782d98c34d4b
carlosx06x <carlos.alberto.bermudez@correounivalle.edu.co>
2023-01-20 00:24:58 -0500

    borrando caracteres de precio

--- webscraping/samsung_cleversel.py
+++ webscraping/samsung_cleversel.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from productos.models import Product
 import save_into_db
+import re
 
 def samsung_cleversel():
    
@@ -14,18 +15,20 @@ def samsung_cleversel():
      print("===================")
      nombre = (div.find("div", class_="product-item-meta").a.string)
      precio = (div.find("span", class_="price").text)
+     patron = '[a-z, A-Z]'
+     result_precio = re.sub(patron, '', precio)
      url_celular = "https://www.clevercel.co"+(div.find("div",class_="product-item__image-wrapper").a["href"])
      url_imagen = "https:"+(div.find("div",class_="product-item__image-wrapper").img["src"])
      base_product_id = save_into_db.get_baseproduct_id(nombre)
      
      print(nombre)
-     print(precio)
+     print(result_precio)
      print(url_celular)
      print(url_imagen)
      
      p = Product(
                     name= nombre,
-                    price= precio,
+                    price= result_precio,
                     url_image=url_imagen,
                     url_origin=url_celular,
                     vendor_address = 'Cra. 51 #183-17 local #2-45, Suba, Bogotá',
