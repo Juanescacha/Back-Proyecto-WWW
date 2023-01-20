ebab240dd54635020e3eebc0c0dd0c0d66a419b9
timetravel-1010 <timetravel_1010@protonmail.com>
2023-01-19 23:37:23 -0500

    Modificar campos para permitir mayor extensión de caracteres

--- productos/models.py
+++ productos/models.py
@@ -3,11 +3,11 @@ from productos_base.models import BaseProduct
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=300)
     price = models.CharField(max_length=50)
-    url_image = models.URLField(max_length=500)
-    url_origin = models.URLField(max_length=500)
-    vendor_address = models.CharField(max_length=300)
+    url_image = models.URLField(max_length=1500)
+    url_origin = models.URLField(max_length=1500)
+    vendor_address = models.CharField(max_length=500)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     base_product = models.ForeignKey(BaseProduct, null=True, on_delete=models.CASCADE)
--- productos/views.py
+++ productos/views.py
@@ -5,7 +5,13 @@ from .models import Product
 from webscraping.wom_bs4 import wom_ws
 from webscraping.xiaomi_store_bs4 import xiaomi_ws
 from webscraping.xiaomi_phonelectrics import xiaomi_phonelectrics
-from webscraping.save_into_db import save_products
+from webscraping.iphone import iphone
+from webscraping.iphone_cleversel import iphone_cleversel
+from webscraping.iphone_merlib import iphone_merlib
+from webscraping.samsung_merlib import samsumg_merlib
+from webscraping.samsung_cleversel import samsung_cleversel
+from webscraping.samsung_phonelectrics import samsung_phonelectrics
+
 
 # Create your views here.
 class ProductApiView(APIView):
@@ -18,9 +24,17 @@ class ProductApiView(APIView):
     def post(self, request):
         Product.objects.all().delete()
 
-        xiaomi_phonelectrics()
+        #iphone() pendiente arreglar los nombres
+        #iphone_cleversel() pendiente mostrar bien los precios
+        iphone_merlib()
+        xiaomi_phonelectrics() # <- iphone 
+
+        samsumg_merlib()
+        #samsung_cleversel() pendiente mostrar bien los precios
+        samsung_phonelectrics()
+
         xiaomi_ws()
-        wom_ws()
+        wom_ws() # <- xiaomi
 
         print('Se ha realizado el webscraping, se guardaron los productos en la base de datos.')
         products = Product.objects.all().values()
