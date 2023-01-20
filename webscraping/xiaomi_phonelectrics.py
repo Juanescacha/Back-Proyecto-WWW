e9820770af8d0b1c3c665e8d86731caf95d9bf44
timetravel-1010 <timetravel_1010@protonmail.com>
2023-01-19 21:00:09 -0500

    Agregar modificaciones a ws de xiaomi en ML

--- webscraping/xiaomi_phonelectrics.py
+++ webscraping/xiaomi_phonelectrics.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 from productos.models import Product
-from webscraping.save_into_db import get_baseproduct_id
+from webscraping import save_into_db
 
-def extraData():
+def xiaomi_phonelectrics():
    
     baseUrl = "https://www.phonelectrics.com/collections/apple-celulares"
     response = requests.get(baseUrl)
@@ -14,13 +14,13 @@ def extraData():
         print("===================")
         nombre = (div.find("div", class_="product-grid--title").a.string)
         precio = (div.find("span", class_="money sale-price").text)
-        url_celular = (div.find("div", class_="grid-view-item-image").a["href"])
-        url_imagen = (div.find("div", class_="grid-view-item-image").img["src"])
-        base_product_id = get_baseproduct_id(nombre)
+        url_celular = "https://www.phonelectrics.com"+(div.find("div", class_="grid-view-item-image").a["href"])
+        url_imagen = "https:"+(div.find("div", class_="grid-view-item-image").img["src"])
+        base_product_id = save_into_db.get_baseproduct_id(nombre)
    
         print(nombre)
         print(precio)
-        print("https://www.phonelectrics.com"+url_celular)
+        print(url_celular)
         print(url_imagen)
         
         p = Product(
@@ -28,7 +28,8 @@ def extraData():
                     price= precio,
                     url_image=url_imagen,
                     url_origin=url_celular,
-                    vendor_address = '',
+                    vendor_address = 'Av El Dorado # 68C - 61 Torre Central Davivienda Bogotá - Colombia',
                     base_product = None if not base_product_id else base_product_id.id,
                 )
         p.save()
+        
